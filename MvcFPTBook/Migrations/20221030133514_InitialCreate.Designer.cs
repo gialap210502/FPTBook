2623e092d3b93bbfcfa77bfdf3898df0a1240427
gialap210502 <89369650+gialap210502@users.noreply.github.com>
2022-10-30 22:07:06 +0700

    Update UI

--- MvcFPTBook/Migrations/20221030133514_InitialCreate.Designer.cs
+++ MvcFPTBook/Migrations/20221030133514_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcFPTBook.Areas.Identity.Data;
 
@@ -11,9 +12,10 @@ using MvcFPTBook.Areas.Identity.Data;
 namespace MvcFPTBook.Migrations
 {
     [DbContext(typeof(MvcFPTBookIdentityDbContext))]
-    partial class MvcFPTBookIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221030133514_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
